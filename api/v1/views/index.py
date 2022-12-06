0fddf4e149640ffa743f68f4f78542c0b1cb7582
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-06 15:45:32 +0100

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,29 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status')
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/status')
+def number_of_object():
+    '''
+    retrieves the number of each objects by type
+    '''
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    dic = {}
+    for i in classes:
+        dic[i] = storage.count(classes[i])
+    
+    return jsonify(dic)
