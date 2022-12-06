ea84658528a5f6b1050801415662b13ba886e879
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-06 15:25:28 +0100

    ya raby te5dem

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+
+"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """
+    calls storage.close(
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    a = getenv("HBNB_API_HOST ", default='0.0.0.0')
+    b = getenv('HBNB_API_PORT', default=5000)
+    app.run(host=a, port=b, threaded=True)
