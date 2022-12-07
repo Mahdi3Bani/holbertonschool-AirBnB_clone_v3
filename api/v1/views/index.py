57b3a69c24a07aeb36d298e685036245eb4c1458
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-07 11:16:49 +0100

    dic or json

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,22 +13,23 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats',  methods=['GET'], strict_slashes=False)
+@app_views.route('/stats',  methods=['GET'], strict_slashes=False)
 def number_of_object():
     '''
     retrieves the number of each objects by type
     '''
     classes = [Amenity, City, Place, Review, State, User]
-    classes_names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    classes_names = ["amenities", "cities",
+                     "places", "reviews", "states", "users"]
 
     dic = {}
-    for i in range (len(classes)):
+    for i in range(len(classes)):
         dic[classes_names[i]] = storage.count(classes[i])
 
     return jsonify(dic)
