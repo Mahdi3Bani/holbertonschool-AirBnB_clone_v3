9268c8ac3fe175a94e2fe3c55769cf5d1271ae3d
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-08 14:58:27 +0100

    done :)

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,14 +6,14 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 app.register_blueprint(app_views)
 
-
+CORS(app, origins="0.0.0.0")
 @app.teardown_appcontext
 def teardown_appcontext(self):
     """
